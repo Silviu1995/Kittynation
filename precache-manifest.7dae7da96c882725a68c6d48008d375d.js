6896420a3c32fa1c3f69ca36f736e9622e16e1ec
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2022-09-05 19:33:08 +0300

    Updates

---
+++ precache-manifest.7dae7da96c882725a68c6d48008d375d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c7fb6b14920095b4015",
+    "url": "/Kittynation/static/css/main.0c67b41f.chunk.css"
+  },
+  {
+    "revision": "3c7fb6b14920095b4015",
+    "url": "/Kittynation/static/js/main.14b8a6b2.chunk.js"
+  },
+  {
+    "revision": "5d34b68f3f9e91b8971f",
+    "url": "/Kittynation/static/js/runtime~main.dedba3b1.js"
+  },
+  {
+    "revision": "6aebd69055693105d4c8",
+    "url": "/Kittynation/static/css/2.cc52d5e7.chunk.css"
+  },
+  {
+    "revision": "6aebd69055693105d4c8",
+    "url": "/Kittynation/static/js/2.3cba9a74.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/Kittynation/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Kittynation/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5869f862aea4108dce325460ba3f6dc8",
+    "url": "/Kittynation/index.html"
+  }
+];
